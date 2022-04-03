6021a8f7bc825a8098fd416733e6d686b37c8a15
OmarHamdann <omar.haamdan@gmail.com>
2022-04-03 13:55:39 +0300

    fix bug

--- db/db.js
+++ db/db.js
@@ -2,17 +2,14 @@ const mysql = require("mysql2");
 
 
 let connection ="";
-try{
-   connection = mysql.createConnection({
+
+   connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
   });
   console.log("Connected");
-}
-catch(err){
-  console.log(err);
-}
+
 
 module.exports = connection;
